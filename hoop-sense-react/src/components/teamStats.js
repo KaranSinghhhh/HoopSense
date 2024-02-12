c0626d4e3f81c45fe9998f816b23cd49a093b7e1
maybekd <karandeepsingh8860@gmail.com>
2024-02-12 18:34:01 -0500

    Updated this file with a new feature

--- hoop-sense-react/src/components/teamStats.js
+++ hoop-sense-react/src/components/teamStats.js
@@ -2,10 +2,19 @@ import React from 'react'
 
 function teamStats() {
   return (
-    <div className='mt-[130px] flex justify-center items-center'>
-        <h1>Team Stats</h1>
+    
+    
+        <div className='mt-[85px] flex justify-center py-3 px-6 bg-gray-50 border-b'>
+            <h1></h1>
+            <form action="">
+                <input type="text" name="search" placeholder="Search for Team" autocomplete="off" aria-label="Search talk" className="px-3 py-2 font-semibold placeholder-gray-500 text-black rounded-2xl border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2 " >
 
-    </div>
+                </input>
+            </form>
+
+        </div>
+    
+    
   )
 }
 
