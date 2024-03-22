44edbbd3795dddc92188d87a2ec18d8baf3f1569
maybekd <karandeepsingh8860@gmail.com>
2024-03-21 21:23:30 -0400

    added the player stat type for player stats

--- hoop-sense-react/src/components/playerStats.js
+++ hoop-sense-react/src/components/playerStats.js
@@ -49,7 +49,6 @@ function PlayerStats() { // Capitalize the first letter to conform to React comp
       {playerStats.map((player, index) => (
   
           <div key={index} className='mb-4'>
-          
             <h1>{player.PLAYER_NAME}</h1>
             <p>Games Played: {player.GP}</p>
             <p>Wins: {player.W} | Loss: {player.L}</p>
@@ -57,7 +56,8 @@ function PlayerStats() { // Capitalize the first letter to conform to React comp
             <div className='mt-[20px] overflow-x-auto'>
               <div style={{ minWidth: 'max-content' }}>
               <ul className={`flex gap-[20px] justify-start items-center`}>
-              <li>Type</li> 
+                <li>Type</li> 
+                <li className="w-[80px] text-center">MIN</li>
                 <li className="w-[80px] text-center">FGM</li>
                 <li className="w-[80px] text-center">FGA</li>
                 <li className="w-[80px] text-center">FG%</li>
@@ -67,9 +67,9 @@ function PlayerStats() { // Capitalize the first letter to conform to React comp
                 <li className="w-[80px] text-center">FTM</li>
                 <li className="w-[80px] text-center">FTA</li>
                 <li className="w-[80px] text-center">FT%</li>
-                <li className="w-[80px] text-center">REB</li>
                 <li className="w-[80px] text-center">OREB</li>
                 <li className="w-[80px] text-center">DREB</li>
+                <li className="w-[80px] text-center">REB</li>
                 <li className="w-[80px] text-center">AST</li>
                 <li className="w-[80px] text-center">TOV</li>
                 <li className="w-[80px] text-center">STL</li>
@@ -77,13 +77,36 @@ function PlayerStats() { // Capitalize the first letter to conform to React comp
                 <li className="w-[80px] text-center">BLKA</li>
                 <li className="w-[80px] text-center">PF</li>
                 <li className="w-[80px] text-center">PFD</li>
-                <li className="w-[80px] text-center">PTS</li>
-                <li className="w-[80px] text-center">+/-</li>                                                                
+                <li className="w-[80px] text-center">PTS</li>    
+                <li className="w-[80px] text-center">+/-</li>                                                             
                                                                                 
               </ul>
               <hr></hr> 
               <ul className='flex gap-[20px] justify-start items-center'>
                 <li>Player</li> 
+                <li className="w-[80px] text-center">{player.MIN}</li>
+                <li className="w-[80px] text-center">{player.FGM}</li>
+                <li className="w-[80px] text-center">{player.FGA}</li>
+                <li className="w-[80px] text-center">{player.FG_PCT}</li>
+                <li className="w-[80px] text-center">{player.FG3M}</li>
+                <li className="w-[80px] text-center">{player.FG3A}</li>
+                <li className="w-[80px] text-center">{player.FG3_PCT}</li>
+                <li className="w-[80px] text-center">{player.FTM}</li>
+                <li className="w-[80px] text-center">{player.FTA}</li>
+                <li className="w-[80px] text-center">{player.FT_PCT}</li>
+                <li className="w-[80px] text-center">{player.OREB}</li>
+                <li className="w-[80px] text-center">{player.DREB}</li>
+                <li className="w-[80px] text-center">{player.REB}</li>
+                <li className="w-[80px] text-center">{player.AST}</li>
+                <li className="w-[80px] text-center">{player.TOV}</li>
+                <li className="w-[80px] text-center">{player.STL}</li>
+                <li className="w-[80px] text-center">{player.BLK}</li>
+                <li className="w-[80px] text-center">{player.BLKA}</li>
+                <li className="w-[80px] text-center">{player.PF}</li>
+                <li className="w-[80px] text-center">{player.PFD}</li>
+                <li className="w-[80px] text-center">{player.PTS}</li>    
+                <li className="w-[80px] text-center">{player.PLUS_MINUS}</li>                                                             
+                                                                    
           
               </ul>
               <hr ></hr>
